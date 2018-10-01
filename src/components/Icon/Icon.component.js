17695dfa3fac724aa0e269581c393d4e7507dc7d
Chris Brett <chris.brett@citizensadvice.org.uk>
2018-10-01 12:08:55 +0100

    Updates

--- src/components/Icon/Icon.component.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Icon = ({ iconType, iconColor }) => {
-  return <span className={`c-icon c-icon--${iconColor} c-icon--${iconType}`} />;
-};
-
-export default Icon;
