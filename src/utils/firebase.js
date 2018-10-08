e21518ad6371c5f47cc2964719589bec0653764a
Chris Brett <chris.brett@citizensadvice.org.uk>
2018-10-08 18:59:35 +0100

    Chore: Replace dev config with production
    Resolves #10 & CMS7-385

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 
-const config = {
+const devConfig = {
   apiKey: "AIzaSyCZSDNPsFaBbwgY0P3tySbPbUr1uD39hgI",
   authDomain: "content-testing.firebaseapp.com",
   databaseURL: "https://content-testing.firebaseio.com",
@@ -9,5 +9,14 @@ const config = {
   messagingSenderId: "895732552641"
 };
 
-firebase.initializeApp(config);
+const prodConfig = {
+  apiKey: "AIzaSyB66xZ4sV3BQSn__zCnCmSOWoH_8squ8Hk",
+  authDomain: "customerjourney-214813.firebaseapp.com",
+  databaseURL: "https://customerjourney-214813.firebaseio.com",
+  projectId: "customerjourney-214813",
+  storageBucket: "customerjourney-214813.appspot.com",
+  messagingSenderId: "1085078162963"
+};
+
+firebase.initializeApp(prodConfig);
 export default firebase;
