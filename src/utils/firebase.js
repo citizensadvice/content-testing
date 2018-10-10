b6c2d07b16eb5c0bd7ec649be3611b103ba1d36d
Chris Brett <chris.brett@citizensadvice.org.uk>
2018-10-10 15:42:19 +0100

    Chore: Reduce file size
    Import only what's needed in order to reduce the final JavaScript file size.
    Firebase JS SDK comes with a raft of utility tools, only importing the necessary functions drastically reduces the overall file size to the application JS.
    
    Resolves #24 & CMS7-385

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import { firebase } from "@firebase/app";
+import "@firebase/database";
 
 const devConfig = {
   apiKey: "AIzaSyCZSDNPsFaBbwgY0P3tySbPbUr1uD39hgI",
